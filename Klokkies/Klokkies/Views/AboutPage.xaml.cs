910c7f2a819e538a7d372f57f026d4ad0a86efd8
Mels <m.c.vanes@tilburguniversity.edu>
2023-02-09 17:53:00 +0100

    Add project files.

---
+++ Klokkies/Klokkies/Views/AboutPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Klokkies.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Klokkies/Klokkies/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Klokkies.Models;
+using Klokkies.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Klokkies.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
